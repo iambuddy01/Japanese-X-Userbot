727ca673604ee132b8c15f897abdaf2c4df9be99
Buddy <203337004+iambuddy01@users.noreply.github.com>
2025-12-08 14:33:38 +0530

    ᴅᴇᴠɴʙᴏᴛs

--- X/modules/user/allertoncastle.py
+++ X/modules/user/allertoncastle.py
@@ -1,6 +1,6 @@
 #MIT License
 
-#Copyright (c) 2024 Japanese-X-Userbot
+#Copyright (c) 2024 deleted-account
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -46,7 +46,7 @@ async def castle(client, message):
     await message.reply_photo(castle_url)
 
 add_command_help(
-    "•─╼⃝𖠁 Aʟʟᴇʀᴛᴏɴ Cᴀsᴛʟᴇ",
+    "allerton",
     [
        ["allerton", "Gɪᴠᴇ random Aʟʟᴇʀᴛᴏɴ Cᴀsᴛʟᴇ pic."],
         ],
