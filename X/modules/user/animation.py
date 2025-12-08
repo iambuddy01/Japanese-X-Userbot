f350cb4272a98969338bff4636e0cab4f4ad794c
Buddy <203337004+iambuddy01@users.noreply.github.com>
2025-12-08 14:46:41 +0530

    ᴅᴇᴠɴʙᴏᴛs

--- X/modules/user/animation.py
+++ X/modules/user/animation.py
@@ -1,6 +1,6 @@
 #MIT License
 
-#Copyright (c) 2024 Japanese-X-Userbot
+#Copyright (c) 2024 deleted-account
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -20,17 +20,6 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-# Credits: @mrismanaziz
-# Copyright (C) 2022 Pyro-ManUserbot
-#
-# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
-#
-# t.me/SharingUserbot & t.me/Lunatic0de
-
-
-#REMAKE BY : NOBITA XD AND TRYTOLIVEALONE
 
 
 import asyncio
@@ -1303,7 +1292,7 @@ async def hearts(client: Client, message: Message):
 
 
 add_command_help(
-    "•─╼⃝𖠁 ᴀɴɪᴍᴀᴛɪᴏɴ",
+    "animation",
     [
         ["fuck", "To display the middle finger animation."],
         ["ror", "To display a ror animation."],
